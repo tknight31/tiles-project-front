d2f9ae116dcc70921a6cd019d0a9cc3c129daec3
jamesrhee91 <jamesshrhee@gmail.com>
2017-08-22 11:06:04 -0400

    abstracted div rows and added event listener

---
+++ src/components/note.js
@@ -0,0 +1,38 @@
+const Row = (function createRowClass() {
+
+  let id = 0
+
+  return class Row {
+
+    constructor() {
+      this.id = ++id
+      this.notes = ["A", "S", "D", "F"]
+      this.num = Math.round(Math.random() * 3)
+      this.selected = (this.notes[this.num]).toLowerCase()
+    }
+
+
+    render() {
+      return `
+      <div class="grid" data-row-id="${this.id}">${this.generateNotes()}</div>
+      `
+    }
+
+    generateNotes() {
+      // let randNum = Math.round(Math.random() * 3)
+      let row = this.notes.map((e, i) => {
+        if (i == this.num) {
+          return `<div class="block selected">${e}</div>`
+        } else {
+          return `<div class="block">${e}</div>`
+        }
+      }).join("")
+
+      return row
+    }
+
+
+  }
+
+
+})()
--- src/index.js
+++ src/index.js
@@ -1,15 +1,45 @@
 document.addEventListener("DOMContentLoaded", function() {
 
   const gameWrapper = document.getElementById('game-wrapper')
+
+  let allRows = []
+  let currentRow = 1
+
+  for (let i = 0; i < 30; i++) {
+    allRows.push(new Row())
+  }
+  gameWrapper.innerHTML = allRows.slice().reverse().map(e => e.render()).join("")
+
+
   let allDivs = gameWrapper.querySelectorAll(".grid")
   let translateValue = -150 * (allDivs.length-3)
   gameWrapper.style.transform = `translateY(${translateValue}px)`
 
-  window.addEventListener('keyup', function(e) {
-    if (e.which == "32") {
-      translateValue += 150
-      gameWrapper.style.transform = `translateY(${translateValue}px)`
-    }
-  })
+
+  // allRows.reverse().forEach(row => {
+
+    window.addEventListener('keydown', function(event) {
+      let currentKey = allRows[currentRow-1]
+      // debugger
+      if (event.key == currentKey.selected && currentKey.id == currentRow) {
+        translateValue += 150
+        gameWrapper.style.transform = `translateY(${translateValue}px)`
+        currentRow++
+        // setTimeout(function() {currentRow++}, 0)
+      } else {
+        console.log("asdfasdfass");
+      }
+
+    })
+
+  // })
+
+
+  // window.addEventListener('keyup', function(e) {
+  //   if (e.which == "32") {
+  //     translateValue += 150
+  //     gameWrapper.style.transform = `translateY(${translateValue}px)`
+  //   }
+  // })
 
 })
